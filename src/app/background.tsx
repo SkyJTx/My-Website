507a18874c07b009d1b0d6ef1ab6e80c1d14048c
SkyJTx <111367081+SkyJTx@users.noreply.github.com>
2023-11-30 20:47:13 +0700

    1.0.0dev0001 - Navigation Bar / Background

---
+++ src/app/background.tsx
@@ -0,0 +1,7 @@
+const Background = () => {
+    return (
+        <div className="fixed bg-gradient-to-tr from-blue-500 via-purple-500 to-pink-500 h-screen w-screen z--1 top-0"/>
+    );
+}
+
+export default Background;
---
+++ src/app/navbar.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+
+const Navbar = () => {
+  const navStyle: string = "nav-link inline-block m-2 p-3 border-transparent hover:text-yellow-400 transition ease-in-out duration-1000"
+
+  return (
+    <div>
+      <nav className="fixed top-0 z-50 w-screen bg-gradient-to-b from-slate-800 to-transparent">
+        <ul>
+          <li id="Home" className={navStyle}>
+            <Link href="/">
+              Home
+            </Link>
+          </li>
+          <li id="Work" className={navStyle}>
+            <Link href="/work">
+              Work
+            </Link>
+          </li>
+          <li id="About" className={navStyle}>
+            <Link href="/about">
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav> 
+    </div> 
+  )
+}
+
+export default Navbar
