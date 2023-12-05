bfc2abf37b89dc64e11aaf9d0379b3b4486aecfd
SkyJTx <111367081+SkyJTx@users.noreply.github.com>
2023-12-05 23:00:20 +0700

    v1.2.0 - recommit 1 - add loading animation to the fetch-required component

--- src/app/component/main/footer.tsx
+++ src/app/component/main/footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
                 }.
             </p>
             <p className="text-center text-lg text-white">
-                SkyJT's Website v1.1.0 | Nattakarn Khumsupha. LaTeX Project Public License v1.3c.
+                SkyJT's Website v1.2.0 | Nattakarn Khumsupha. LaTeX Project Public License v1.3c.
             </p>
         </footer>
     )
