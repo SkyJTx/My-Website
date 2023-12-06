f32c6d16f4f1831db73a260984b549e22ee9028b
SkyJTx <111367081+SkyJTx@users.noreply.github.com>
2023-12-06 23:28:38 +0700

    v1.2.1 - fix loading in github repo fetch

--- src/app/component/github_repo/repoclient.tsx
+++ src/app/component/github_repo/repoclient.tsx
@@ -4,7 +4,7 @@ import { RepoListProps } from "./interface"
 const RepoClient = ({data}: RepoListProps) => {
     return (
         <div className="p-10 overflow-auto rounded-xl" id="music-repositories">
-            {data !== undefined ? (data.map((repo, index: number) => (
+            {data.length != 0 ? (data.map((repo, index: number) => (
                 <div className="m-5 p-5 border-2 border-solid border-white rounded-md hover:scale-105 hover:bg-slate-700 transition-transform ease-in-out" id={repo.name} key={index}>
                     <a className="hyperlink text-xl w-fit" href={repo.url} target="_blank" rel="noopener noreferrer">
                         {(index+1) + ". " + repo.name}
